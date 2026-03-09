a0c5cccb93f5f98ec0dcc6958ec6bd7653ea2649
LuanDaniel021 <luancordeiro021@gmail.com>
2026-03-09 15:52:16 -0300

    V2.9.4

--- JDM/src/com/jdm/engine/Struct.java
+++ JDM/src/com/jdm/engine/Struct.java
@@ -2,9 +2,7 @@ package com.jdm.engine;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import com.jdm.engine.Link.Linker;
 
@@ -127,75 +125,6 @@ class Struct implements Builder<Struct> {
 		count++;
 		
 		return String.format("%s-%d", key, count);
-	}
-	
-	class Root {
-		
-		public Map<String, Boolean> persists;
-
-		public Set<String> posibles;
-
-		public String _default;
-
-		public String now;
-		
-		{
-			persists = new HashMap<>();
-			posibles = new HashSet<>();
-			_default = "_root_";
-			now = "";
-		}
-		
-		public Root(Field[] fields) {
-
-			Class<Parent> p = Parent.class;
-
-			String field = "";
-
-			for ( Field f : fields ) {
-
-				if ( p.isAssignableFrom( f.getType() ) ) { String name = f.getName();
-
-					if (field.isEmpty()) field = name;
-
-					posibles.add( name );
-
-					persists.put(name, false);
-
-				}
-
-			}
-			
-			if ( !posibles.contains(_default) ) {
-
-				_default = field;
-
-			}
-			
-			now = _default;
-
-		}
-	
-	}
-	
-	class Data {
-		
-		String _id;
-
-		String[] _class;
-
-		Layout _layout;
-
-		Contrains _contrains;
-		
-	}
-	
-	class Layout {
-		
-	}
-	
-	class Contrains {
-		
 	}
 
 }
